a3660b2191b54878723b19e1378b65ab73e6f567
Christoffer Grännby <cribbster1987@hotmail.com>
2021-05-21 13:40:51 +0200

    more docker-compose and properties files added

--- src/main/java/com/example/springdocker/model/Food.java
+++ src/main/java/com/example/springdocker/model/Food.java
@@ -3,10 +3,12 @@ package com.example.springdocker.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+//@Accessors(chain = true)
 public class Food {
     String id;
     String name;
